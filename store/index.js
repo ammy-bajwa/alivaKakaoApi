bd17dc702c56da5aed231a2389f985bcadb6ed4a
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2021-05-19 08:46:47 +0500

    Adding on message

--- store/index.js
+++ store/index.js
@@ -11,6 +11,17 @@ module.exports = {
   },
   setClient(clientInstance) {
     this.client = clientInstance;
+    clientInstance.on("chat", (data, channel) => {
+      console.log(("chat data: ", data));
+      const sender = data.getSenderInfo(channel);
+      if (!sender) return;
+
+      if (data.text === "self") {
+        channel.sendChat("Hello from amir");
+      } else {
+        this.ws;
+      }
+    });
   },
   getClient() {
     return this.client;
