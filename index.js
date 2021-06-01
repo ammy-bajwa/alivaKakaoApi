32a841815c9ddd0136a7c0b5d3556d6dcc64b8d7
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2021-06-01 09:52:36 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const WebSocket = require("ws");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4030;
 
 const Login = require("./routes/login");
 const Device = require("./routes/device");
