1d3aab51eeaf7dd39a7301d19d3bbee1cf8f917b
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2021-06-25 03:39:43 +0500

    Converting image to base 64

--- routes/media.js
+++ routes/media.js
@@ -2,22 +2,25 @@ const express = require("express");
 const router = express.Router();
 const fetch = require("node-fetch");
 const fs = require("fs");
+const path = require("path");
 
-router.post("/", async (req, res) => {
-  fetch(
-    "http://th-m1.talk.kakao.com/th/talkm/oYpU69JQce/HEGxQisvWXKKiVoINxRCk1/i_077d75503acd_120x120.jpg"
-  )
-    .then((res) => res.blob())
-    .then(async (body) => {
-      console.log("body: ");
-      const fileBuffer = await body.arrayBuffer();
-      var imageName = "uploads/test.png";
-      const result = fs
-        .createWriteStream(imageName)
-        .write(new Uint8Array(fileBuffer));
-      console.log("result: ", result);
-      //   res.sendFile(body);
-    });
+router.get("/", async (req, res) => {
+  //   fetch(
+  //     "http://th-m1.talk.kakao.com/th/talkm/oYpU69JQce/HEGxQisvWXKKiVoINxRCk1/i_077d75503acd_120x120.jpg"
+  //   )
+  //     .then((res) => res.blob())
+  //     .then(async (body) => {
+  //       console.log("body: ");
+  //       const fileBuffer = await body.arrayBuffer();
+  //       var imageName = "uploads/test.png";
+  //       const result = fs
+  //         .createWriteStream(imageName)
+  //         .write(new Uint8Array(fileBuffer));
+  //     });
+
+  var imageName = "uploads/test.png";
+  console.log("result: ", path.resolve(__dirname, "..", imageName));
+  res.sendFile(path.resolve(__dirname, "..", imageName));
 });
 
 //export this router to use in our server.js
