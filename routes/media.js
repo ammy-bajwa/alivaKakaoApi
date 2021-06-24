fdf1db23d3b4eed42b31b858f6d1d3f3a234cb60
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2021-06-25 03:16:11 +0500

    Downloading and saving image on server

--- routes/media.js
+++ routes/media.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const fetch = require("node-fetch");
+const fs = require("fs");
 
 router.post("/", async (req, res) => {
   fetch(
     "http://th-m1.talk.kakao.com/th/talkm/oYpU69JQce/HEGxQisvWXKKiVoINxRCk1/i_077d75503acd_120x120.jpg"
   )
     .then((res) => res.blob())
-    .then((body) => {
-      console.log("body: ", body);
-      res.json({
-        data: "ok",
-        success: true,
-      });
+    .then(async (body) => {
+      console.log("body: ");
+      const fileBuffer = await body.arrayBuffer();
+      var imageName = "uploads/test.png";
+      const result = fs
+        .createWriteStream(imageName)
+        .write(new Uint8Array(fileBuffer));
+      console.log("result: ", result);
+      //   res.sendFile(body);
     });
 });
 
