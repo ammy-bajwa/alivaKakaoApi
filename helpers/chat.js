f04d8e541f114e085b05034de367fb29706d223c
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2021-06-29 11:21:16 -0700

    Update chat.js

--- helpers/chat.js
+++ helpers/chat.js
@@ -43,10 +43,14 @@ const getAllMessages = async (
             ) {
               receivedMessageObj.attachment.thumbnailUrlBase64 =
                 await downloadFile(receivedMessageObj.attachment.thumbnailUrl);
-
               receivedMessageObj.attachment.urlBase64 = await downloadFile(
                 receivedMessageObj.attachment.url
               );
+
+              const mediaTypeArr =
+                receivedMessageObj.attachment.thumbnailUrl.split(".");
+              const mediaType = mediaTypeArr[mediaTypeArr.length - 1];
+              console.log("mediaType: ", mediaType);
             }
             const msgObj = {
               text: receivedMessageObj.text,
