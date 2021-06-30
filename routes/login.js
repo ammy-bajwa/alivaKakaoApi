f1a4c008cf53f91afb4aac74023a0b0bde78ac19
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2021-06-30 11:10:27 +0500

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -6,8 +6,9 @@ const store = require("../store/index");
 const {
   AuthApiClient,
   TalkClient,
-  KnownAuthStatusCode,
+  // KnownAuthStatusCode,
   OAuthApiClient,
+  // ServiceApiClient,
 } = require("node-kakao");
 const { getAllMessages } = require("../helpers/chat");
 const { causeDelay } = require("../helpers/delay");
@@ -66,6 +67,7 @@ router.post("/", async (req, res) => {
       if (!response.success) {
         console.log("Second");
         for (let index = 0; index < 5; index++) {
+          causeDelay(2000);
           const oAuthClient = OAuthApiClient.create();
           const {
             result: {
@@ -88,7 +90,7 @@ router.post("/", async (req, res) => {
             break;
           } else {
             // console.log("accessToken: ", accessToken);
-            // console.log("response: ", response);
+            console.log("response: ", response);
           }
         }
         if (!response.success) {
@@ -97,124 +99,124 @@ router.post("/", async (req, res) => {
             error: response.status,
             message: "Failed to login",
           });
-          return;
         }
-      }
-      const allList = client.channelList.all();
-      let chatList = {};
-      let messages = {};
-      let storeChatList = {};
-      const messageStore = [];
-      const loggedInUserId = parseInt(response.result.userId);
-      let largestTimeStamp = lastMessageTimeStamp;
-      let biggestChatLog = latestLogId;
-      try {
-        for (const item of allList) {
-          const { displayUserList, lastChatLogId, newChatCount } = item.info;
-          const { nickname, userId } = displayUserList[0];
-          const currentUserId = parseInt(userId);
-          messages[nickname] = {
-            userId: currentUserId,
-            messages: [],
-          };
-          let itemChat = [];
-          if (parseInt(lastChatLogId) > latestLogId) {
-            const { newMessages, latestTimeStamp } = await getAllMessages(
-              item,
-              lastChatLogId,
-              latestLogId,
-              client.clientUser.userId,
-              email,
-              nickname,
-              lastMessageTimeStamp
-            );
-            itemChat = newMessages;
-            if (latestTimeStamp > largestTimeStamp) {
-              largestTimeStamp = latestTimeStamp;
-            }
-            if (parseInt(lastChatLogId) > biggestChatLog) {
-              biggestChatLog = parseInt(lastChatLogId);
-            }
-          }
-          chatList[nickname] = {
-            ...item.info,
-            messages: itemChat,
-            intId: currentUserId,
-            newChatCount,
-            lastChatLogId: parseInt(lastChatLogId),
-          };
-          storeChatList[nickname] = item;
-        }
-      } catch (error) {
-        console.error(error);
-      }
-      if (response.success) {
-        store.setClient(email, client);
-        console.log(`Login Success`);
-        res.json({
-          email,
-          loggedInUserId,
-          accessToken: loginRes.result.accessToken,
-          refreshToken: loginRes.result.refreshToken,
-          chatList,
-          messageStore,
-          messages,
-          largestTimeStamp,
-          biggestChatLog,
-        });
-        store.addChatList(email, storeChatList);
-        client.on("chat", (data, channel) => {
-          const sender = data.getSenderInfo(channel);
-          if (!sender) {
-            return;
-          } else {
-            const {
-              _chat: {
-                text,
-                sendAt,
-                attachment,
-                logId,
-                sender: { userId },
-              },
-            } = data;
-            const senderIntUserId = parseInt(userId);
-            const info = channel.getAllUserInfo();
-            let receiverUser = {};
-            let senderUser = {};
-            try {
-              for (const item of info) {
-                const { userId } = item;
-                const currentUserIntId = parseInt(userId);
-                if (senderIntUserId === currentUserIntId) {
-                  senderUser = item;
-                  senderUser.intId = currentUserIntId;
-                } else {
-                  receiverUser = item;
-                  receiverUser.intId = parseInt(userId);
-                }
-              }
-            } catch (error) {
-              console.error(error);
-            }
-            const messageData = {
-              key: "newMesssage",
-              text,
-              sender,
-              logId: parseInt(logId),
-              attachment,
-              receiverUser,
-              sendAt,
-            };
-            const ws = store.getConnection(email);
-            ws.send(JSON.stringify(messageData));
-          }
-        });
       } else {
-        console.error("Client login failed: ", response);
-        res.json({
-          error: response,
-          message: "Failed to login in Kakao talk",
-        });
+        const allList = client.channelList.all();
+        let chatList = {};
+        let messages = {};
+        let storeChatList = {};
+        const messageStore = [];
+        const loggedInUserId = parseInt(response.result.userId);
+        let largestTimeStamp = lastMessageTimeStamp;
+        let biggestChatLog = latestLogId;
+        try {
+          for (const item of allList) {
+            const { displayUserList, lastChatLogId, newChatCount } = item.info;
+            const { nickname, userId } = displayUserList[0];
+            const currentUserId = parseInt(userId);
+            messages[nickname] = {
+              userId: currentUserId,
+              messages: [],
+            };
+            let itemChat = [];
+            if (parseInt(lastChatLogId) > latestLogId) {
+              const { newMessages, latestTimeStamp } = await getAllMessages(
+                item,
+                lastChatLogId,
+                latestLogId,
+                client.clientUser.userId,
+                email,
+                nickname,
+                lastMessageTimeStamp
+              );
+              itemChat = newMessages;
+              if (latestTimeStamp > largestTimeStamp) {
+                largestTimeStamp = latestTimeStamp;
+              }
+              if (parseInt(lastChatLogId) > biggestChatLog) {
+                biggestChatLog = parseInt(lastChatLogId);
+              }
+            }
+            chatList[nickname] = {
+              ...item.info,
+              messages: itemChat,
+              intId: currentUserId,
+              newChatCount,
+              lastChatLogId: parseInt(lastChatLogId),
+            };
+            storeChatList[nickname] = item;
+          }
+        } catch (error) {
+          console.error(error);
+        }
+        if (response.success) {
+          store.setClient(email, client);
+          console.log(`Login Success`);
+          res.json({
+            email,
+            loggedInUserId,
+            accessToken: loginRes.result.accessToken,
+            refreshToken: loginRes.result.refreshToken,
+            chatList,
+            messageStore,
+            messages,
+            largestTimeStamp,
+            biggestChatLog,
+          });
+          store.addChatList(email, storeChatList);
+          client.on("chat", (data, channel) => {
+            const sender = data.getSenderInfo(channel);
+            if (!sender) {
+              return;
+            } else {
+              const {
+                _chat: {
+                  text,
+                  sendAt,
+                  attachment,
+                  logId,
+                  sender: { userId },
+                },
+              } = data;
+              const senderIntUserId = parseInt(userId);
+              const info = channel.getAllUserInfo();
+              let receiverUser = {};
+              let senderUser = {};
+              try {
+                for (const item of info) {
+                  const { userId } = item;
+                  const currentUserIntId = parseInt(userId);
+                  if (senderIntUserId === currentUserIntId) {
+                    senderUser = item;
+                    senderUser.intId = currentUserIntId;
+                  } else {
+                    receiverUser = item;
+                    receiverUser.intId = parseInt(userId);
+                  }
+                }
+              } catch (error) {
+                console.error(error);
+              }
+              const messageData = {
+                key: "newMesssage",
+                text,
+                sender,
+                logId: parseInt(logId),
+                attachment,
+                receiverUser,
+                sendAt,
+              };
+              const ws = store.getConnection(email);
+              ws.send(JSON.stringify(messageData));
+            }
+          });
+        } else {
+          console.error("Client login failed: ", response);
+          res.json({
+            error: response,
+            message: "Failed to login in Kakao talk",
+          });
+        }
       }
     }
   } catch (error) {
